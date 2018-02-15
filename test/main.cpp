29e6309bc4dc3d7394db05bdefa11816d935770c
Dobid <davidolivares68@gmail.com>
2018-02-15 11:46:45 +0100

    Addition Feature

--- test/main.cpp
+++ test/main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main()
 {
     cout << "bye bye world !" << endl;
+    int a,b,c;
+    cout << "type two numbers : " << endl;
+    cin >> a >> b;
+    c = a+b;
+    cout << "the addition between those two numbers results in : " << c << endl;
     return 0;
 }
